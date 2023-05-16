c070c48976826c200b034f247f83982905f9df7d
endyd9 <endyd9@gmail.com>
2023-05-16 16:43:38 +0900

    1일차

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,38 @@
-console.log("hello");
+const ul = document.querySelector("ul");
+const msgForm = document.querySelector("#msg");
+const nickForm = document.querySelector("#nick");
+const ws = new WebSocket(`ws://${window.location.host}`);
+
+ws.addEventListener("open", () => {
+  console.log("Connected✅");
+});
+
+ws.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = `${message.data}`;
+  ul.appendChild(li);
+});
+
+ws.addEventListener("close", () => {
+  console.log("disconnected❌");
+});
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+nickForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nick = nickForm.querySelector("input");
+  ws.send(makeMessage("nickname", nick.value));
+  nick.value = "";
+  nickForm.querySelector("button").innerText = "Change name";
+});
+
+msgForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const message = msgForm.querySelector("input");
+  ws.send(makeMessage("new_message", message.value));
+  message.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
-import path from "path";
+import http from "http";
+import { WebSocketServer } from "ws";
 
 const app = express();
 
@@ -7,11 +8,40 @@ const port = 3000;
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use("/public", express.static(process.cwd() + "/src/public/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => {
   console.log(`server on port ${port}`);
 };
-app.use("/public", express.static(process.cwd() + "/src/public/"));
-app.get("/", (req, res) => res.render("home"));
+const server = http.createServer(app);
+const ws = new WebSocketServer({ server });
 
-app.listen(port, handleListen);
+const onSocketClose = () => {
+  console.log("Disconnected❌");
+};
+
+const sockets = [];
+
+ws.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "무명";
+  console.log("Connected!✅");
+  socket.on("close", () => onSocketClose);
+  socket.on("message", (message) => {
+    const msg = JSON.parse(message);
+    switch (msg.type) {
+      case "new_message":
+        sockets.forEach((sk) => {
+          sk.send(`${socket.nickname}: ${msg.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = msg.payload;
+        break;
+    }
+  });
+});
+
+server.listen(port, handleListen);
